d5a5159a0e3b14960396e25aa9e53d8768e7ad88
Meetu Maltiar <mmaltiar@inphina.com>
2011-12-20 10:26:33 +0530

    refactoring name to a Grid

--- src/test/java/com/meetu/gameoflife/NeighbourhoodTest.java
+++ src/test/java/com/meetu/gameoflife/NeighbourhoodTest.java
@@ -9,17 +9,17 @@ public class NeighbourhoodTest {
 
     @Test
     public void neighbourhood_should_contain_8_neighbours() throws Exception {
-        Universe board = mock(Grid.class);
-        Neighbourhood neighbourhood = new Neighbourhood(board, new Coordinate(0, 0));
+        Universe grid = mock(Grid.class);
+        Neighbourhood neighbourhood = new Neighbourhood(grid, new Coordinate(0, 0));
         assertEquals(8, neighbourhood.neighboursCount());
     }
 
     @Test
     public void neighbourhood_should_check_8_coordinates_when_asked_for_live_count() throws Exception {
-        Universe board = mock(Grid.class);
-        Neighbourhood neighbourhood = new Neighbourhood(board, new Coordinate(0, 0));
+        Universe grid = mock(Grid.class);
+        Neighbourhood neighbourhood = new Neighbourhood(grid, new Coordinate(0, 0));
         neighbourhood.liveCount();
-        verify(board, times(8)).isAlive(isA(Coordinate.class));
+        verify(grid, times(8)).isAlive(isA(Coordinate.class));
     }
 
 
